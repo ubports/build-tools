6b4ccd54b92642184f8968229a98f8a2f40fa912
Marius Gripsgard <marius@ubports.com>
2017-10-21 04:15:25 +0200

    encapsulate REPOSITORY_EXTRA

--- build-binary.sh
+++ build-binary.sh
@@ -22,7 +22,7 @@ export distribution=$(cat distribution.buildinfo)
 
 generate_repo_extra.py
 if [ -f ubports.repos_extra ]; then
-  export REPOSITORY_EXTRA=$(cat ubports.repos_extra)
+  export REPOSITORY_EXTRA="$(cat ubports.repos_extra)"
   export REPOSITORY_EXTRA_KEYS="http://repo.ubports.com/keyring.gpg"
   echo "INFO: Adding extra repo $REPOSITORY_EXTRA"
 fi
