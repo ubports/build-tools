122a3905f7588f97b4dc3ccb5d94db89ca9dc7a0
Marius Gripsgard <marius@ubports.com>
2017-10-21 04:12:47 +0200

    Disable ppa and add branch info file

--- build-source.sh
+++ build-source.sh
@@ -13,29 +13,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
-
 DIST="vivid xenial"
 DIST_DEV="vivid-dev xenial-dev"
 
 if [ ! "$SKIP_MOVE" = "true" ]; then
-	tmp=$(mktemp -d)
-	mv * .* $tmp/
-	mv $tmp ./source
+        tmp=$(mktemp -d)
+        mv * .* $tmp/
+        mv $tmp ./source
 fi
 
 ls source
+
+set -ex
 
 cd source
 export GIT_COMMIT=$(git rev-parse HEAD)
 export GIT_BRANCH=$BRANCH_NAME
 cd ..
+
 if echo $DIST | grep -w $GIT_BRANCH > /dev/null; then
-	echo "This is on a release branch, overriding dist to $GIT_BRANCH"
-	export DIST_OVERRIDE=$GIT_BRANCH
+        echo "This is on a release branch, overriding dist to $GIT_BRANCH"
+        export DIST_OVERRIDE=$GIT_BRANCH
 fi
 if echo "vivid-dev" | grep -w $GIT_BRANCH > /dev/null; then
-	echo "This is on a release branch, overriding dist to vivid"
+        echo "This is on a release branch, overriding dist to vivid"
         export DIST_OVERRIDE="vivid"
 fi
 if echo "xenial-dev" | grep -w $GIT_BRANCH > /dev/null; then
@@ -43,12 +44,17 @@ if echo "xenial-dev" | grep -w $GIT_BRANCH > /dev/null; then
         export DIST_OVERRIDE="xenial"
 fi
 /usr/bin/generate-git-snapshot
-if echo $DIST | grep -w $GIT_BRANCH > /dev/null; then
-	echo "dputing"
-	dput ppa:ubports-developers/overlay *.changes
-fi
-if echo $DIST_DEV | grep -w $GIT_BRANCH > /dev/null; then
-	echo "dev dputing"
-	dput ppa:ubports-developers/overlay-dev *.changes
-fi
+echo "Gen git snapshot done"
+
+# Disable ppa since ppa does not build vivid anymore....
+#if echo $DIST | grep -w $GIT_BRANCH > /dev/null; then
+#        echo "dputing"
+        #dput ppa:ubports-developers/overlay *.changes
+#fi
+#if echo $DIST_DEV | grep -w $GIT_BRANCH > /dev/null; then
+#        echo "dev dputing"
+        #dput ppa:ubports-developers/overlay-dev *.changes
+#fi
+
 rm *.changes
+echo "$GIT_BRANCH" > branch.buildinfo
