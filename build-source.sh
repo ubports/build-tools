f8d1c5f05a3bbd591daba3e62eb7d51ad1cd101f
Marius Gripsgard <mariogrip@ubuntu.com>
2018-02-03 19:31:33 +0100

    Add newer ubuntu releases

--- build-source.sh
+++ build-source.sh
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DIST="vivid xenial"
+DIST="vivid xenial artful bionic"
 DIST_DEV="vivid-dev xenial-dev"
 
 if [ ! "$SKIP_MOVE" = "true" ]; then
