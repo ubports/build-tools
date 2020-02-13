8f052464251707d823e749a3b53be72a3ef79d99
Florian Leeber <flori@bin.org.in>
2020-02-13 21:03:13 +0100

    Fix some python hangover

--- promote-images.py
+++ promote-images.py
@@ -19,6 +19,7 @@
 
 import argparse
 import datetime
+import json
 import os
 import subprocess
 import time
@@ -30,7 +31,7 @@ PUSH_BROADCAST_URL = 'https://push.ubports.com/broadcast'
 PUSH_DATA = '''{
     "channel": "system",
     "expire_on": "%s",
-    "data": "%s"
+    "data": %s
 }'''
 
 
@@ -182,7 +183,8 @@ for device in devices:
             sys.exit(result.returncode)
         new_version = int(result.stdout)
         print("Sending broadcast push notification for device '{}' on channel '{}' and version '{}'".format(device, args.destination_channel, new_version))
-        pushData = '{"{}/{}": [{}, ""]}'.format(args.destination_channel, device, new_version)
+        identifier = "{}/{}".format(args.destination_channel, device)
+        pushData = json.dumps({identifier: [new_version, ""]})
         expiresTime = datetime.datetime.now() + datetime.timedelta(days=1)
         r = requests.post(
             PUSH_BROADCAST_URL,
