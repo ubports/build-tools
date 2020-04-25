28b837c98f31e6d4bdaed5662f0e4d5fe7469e42
Dalton Durst <dalton@ubports.com>
2020-04-25 12:35:15 -0500

    Automate the click importer
    Previously, click_importer.py required its user to manually create the
    click_list and click_list.arm64 files after it finished. These files
    point our rootfs builder to all of the click files needed for its
    current build. This meant that it could not be automated very well, and
    it has been causing issues that would only be detected and resolved
    very occasionally.
    
    click_importer.py also had poor knowledge of different architectures.
    These changes add the '--architecture' argument to allow us to download
    clicks in the architectures we need, today and tomorrow. I've also made
    the apps_ctrl.ini file format take architecture into account, fixing
    a bug where the different architectures of an app could fail to be
    updated depending on the order they were uploaded to the OpenStore.
    
    I've generally linted and reformatted the file as well:
    
    * Switch the % operator out for the str.format() method.
    * Reformat with Black.
    * Took many repetetive actions out and replaced them with variables
    * Removed the dry-run switch. The script modified the apps_ctrl.ini file
      even in dry-run mode, which could cause problems for subsequent runs.
      We did not use it anyway.

--- click_importer.py
+++ click_importer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # Copyright (C) 2017 Marius Gripsgard <marius@ubports.com>
+# Copyright (C) 2020 UBports Foundation
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,14 +16,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# I import clicks from the openstore
+"""
+Script to import and cache clicks from the OpenStore into a filesystem directory
+"""
 
-import argparse, requests, tqdm, os, configparser
+import argparse
+import configparser
+import os
+
+import requests
+import tqdm
+
 
 class Ctrl(object):
     def __init__(self, path):
         self.path = path
-        self.file = os.path.join(path, 'apps_ctrl.ini')
+        self.file = os.path.join(path, "apps_ctrl.ini")
         self.read()
 
     def get(self):
@@ -40,7 +49,7 @@ class Ctrl(object):
         return False
 
     def write(self):
-        with open(self.file, 'w') as f:
+        with open(self.file, "w") as f:
             self.config.write(f)
 
     def read(self):
@@ -52,54 +61,122 @@ class Ctrl(object):
         self.config = config
 
 
-def get_app_info(app):
-    r = requests.get(APPS_API+app)
+def get_app_info(app, architecture):
+    r = requests.get(APPS_API + app, params={"architecture": architecture})
     if not r.status_code == 200:
         return False
     return r.json()["data"]
 
-def download_app(url, id, arch, dest):
-    fileName = "%s_%s.click" % (id, arch)
+
+def download_app(url, id, arch, dest, name):
+    """Downloads an app using the OpenStore API
+
+    :param url: Full URL to the app download. Redirects will be resolved.
+
+    :id: App ID (for example, 'com.ubuntu.calculator').
+
+    :arch: Architecture to download ('all', 'armhf', 'arm64'...).
+
+    :dest: Working directory *and* destination directory for the file. The file
+    will be downloaded into this directory with a temporary name then
+    renamed.
+
+    :name: Final filename for the downloaded app. The temporary download will
+    be renamed to this.
+
+    :returns: Full path to the downloaded file, or None if the file failed to
+    download.
+    """
+    end_file_path = "{}/{}".format(dest, name)
+    temporary_file_path = end_file_path + ".tmp"
+
     request = requests.get(url, stream=True)
     try:
-        total_length = int(request.headers.get('content-length'))
-    except Exception as e:
+        total_length = int(request.headers.get("content-length"))
+    except Exception:
         print("failed to download")
         return
     if not os.path.exists(dest):
         os.makedirs(dest)
-    if os.path.isfile("%s/%s.tmp" % (dest, fileName)):
-        os.remove("%s/%s.tmp" % (dest, fileName))
-    with open("%s/%s.tmp" % (dest, fileName), "wb") as handle:
-        for data in tqdm.tqdm(request.iter_content(chunk_size=1024), total=total_length/1024, leave=True, unit='B', unit_scale=True):
+    if os.path.isfile(temporary_file_path):
+        os.remove(temporary_file_path)
+    with open(temporary_file_path, "wb") as handle:
+        for data in tqdm.tqdm(
+            request.iter_content(chunk_size=1024),
+            total=total_length / 1024,
+            leave=True,
+            unit="B",
+            unit_scale=True,
+        ):
             handle.write(data)
-    os.rename("%s/%s.tmp" % (dest, fileName), "%s/%s" % (dest, fileName))
-    return "%s/%s" % (dest, fileName)
+    os.rename(temporary_file_path, end_file_path)
+    return end_file_path
 
-DEFAULT_DIR="clicks"
-BASE_API="http://open-store.io/api/v3/"
-APPS_API=BASE_API+"apps/"
 
-parser = argparse.ArgumentParser(description='I import clicks from the openstore')
+def write_click_list(list_location, clicks):
+    """Creates a list of clicks to be downloaded by the rootfs builder
+
+    :param list_location: The click list will be placed at this location.
+
+    :param clicks: List of filenames to enter into the click list.
+
+    As of this writing, the script responsible for parsing this list was
+    https://github.com/ubports/livecd-rootfs/blob/xenial/live-build/ubuntu-touch/hooks/60-install-click.chroot
+    """
+    print("Writing final click list to " + list_location)
+
+    with open(list_location, "w") as click_list:
+        click_list.write("\n".join(clicks))
+
+
+def click_name(app_id, version, architecture):
+    """Rebuilds a click's expected filename.
+
+    Needed because the store does not give us information about filenames in
+    downloads, it is expected that the API redirects the browser
+    appropriately.
+    """
+    return "{app_id}_{version}_{architecture}.click".format(
+        app_id=app_id, version=version, architecture=architecture
+    )
+
+
+DEFAULT_DIR = "clicks"
+BASE_API = "http://open-store.io/api/v3/"
+APPS_API = BASE_API + "apps/"
+
+
+parser = argparse.ArgumentParser(description="I import clicks from the openstore")
 parser.add_argument("--dir", "-o", help="output directory", type=str)
-parser.add_argument("--channel", "-c", help="channel to download from (default xenial)", type=str)
-parser.add_argument("--dry", "-n", help="do a dry run", action='store_true')
+parser.add_argument(
+    "--channel",
+    "-c",
+    help="channel to download from (default=xenial)",
+    type=str,
+    default="xenial",
+)
+parser.add_argument(
+    "--architecture",
+    help="Architecture to download (default=armhf)",
+    type=str,
+    default="armhf",
+)
+
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("--file", "-f", help="file with list of apps to import", type=str)
 group.add_argument("--apps", "-a", nargs="+", help="apps to import")
 
+
 args = parser.parse_args()
 
-output_dir = DEFAULT_DIR;
-channel = "xenial"
+output_dir = DEFAULT_DIR
+channel = args.channel
 apps = args.apps
-
-if args.channel:
-    channel = args.channel
+arch = args.architecture
 
 if args.dir:
     if not os.path.exists(args.dir):
-        print("Directory %s do not exist" % args.dir)
+        print("Directory {} does not exist".format(args.dir))
         exit()
     output_dir = args.dir
 
@@ -112,27 +189,48 @@ if args.file:
 
 ctrl = Ctrl(output_dir)
 
+click_list = []
+
 for app in apps:
-    app_info = get_app_info(app)
+    app_info = get_app_info(app, arch)
     if app_info:
         download_found = False
         for download in app_info["downloads"]:
             if download["channel"] == channel:
                 download_found = download
 
-        if (download_found):
-            if ctrl.isNew(app_info["id"], download_found["revision"]):
-                print("New version of %s" % app_info["name"])
+        app_id = app_info["id"]
+        app_name = app_info["name"]
+        if download_found:
+            download_version = download_found["version"]
+            download_architecture = download_found["architecture"]
+            download_revision = download_found["revision"]
+            download_file_name = click_name(
+                app_id, download_version, download_architecture
+            )
+            download_url = download_found["download_url"]
+            control_appid = "{}_{}".format(app_id, download_architecture)
+
+            click_list.append(download_file_name)
+            if ctrl.isNew(control_appid, download_revision):
+                print("New version of {}".format(app_name))
             else:
-                print("No new version of %s" % app_info["name"])
+                print("No new version of {}".format(app_name))
                 continue
-            if args.dry:
-                print("downloading %s" % app_info["name"])
-            else:
-                download_app(download_found["download_url"], app_info["id"],
-                             app_info["architecture"], output_dir)
-            ctrl.update(app_info["id"], download_found["revision"])
+            download_app(
+                download_url,
+                app_id,
+                download_architecture,
+                output_dir,
+                download_file_name,
+            )
+            ctrl.update(control_appid, download_revision)
         else:
-            print("Could not find %s in channel %s" % (app_info["name"], channel))
+            print("Could not find {} in channel {}".format(app_name, channel))
     else:
-        print("Could find fined app %s... ignoring" % app)
+        print("Could find app {} for architecture {}... ignoring".format(app, arch))
+
+if arch == "armhf":
+    write_click_list(output_dir + "/click_list", click_list)
+else:
+    write_click_list(output_dir + "/click_list.{}".format(arch), click_list)
