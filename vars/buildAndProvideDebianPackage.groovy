cc16a835a0958a73fa15f6f923a0ccfa42bf95b8
Florian Leeber <florian@ubports.com>
2021-06-10 00:34:25 +0200

    lets try to fix the message passing (#75)

--- vars/buildAndProvideDebianPackage.groovy
+++ vars/buildAndProvideDebianPackage.groovy
@@ -143,7 +143,8 @@ def call(Boolean isArchIndependent = false, List ignoredArchs = []) {
 
 def notifyTelegram(String message) {
   withCredentials([usernamePassword(credentialsId: 'a25d8b20-4a81-43e9-ac37-dcfb5285790a', usernameVariable: 'TELEGRAM_BOT_CREDS_USR', passwordVariable: 'TELEGRAM_BOT_CREDS_PWD')]) {
-    sh('curl -s -X POST https://api.telegram.org/$TELEGRAM_BOT_CREDS_PWD/sendMessage -d chat_id=$TELEGRAM_BOT_CREDS_USR -d text="${message}"')
+    env['TELEGRAM_BOT_MSG'] = message
+    sh('curl -s -X POST https://api.telegram.org/$TELEGRAM_BOT_CREDS_PWD/sendMessage -d chat_id=$TELEGRAM_BOT_CREDS_USR -d text="$TELEGRAM_BOT_MSG"')
   }
 }
 
