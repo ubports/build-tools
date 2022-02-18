ubports/build-tools

commit efe9052f834d1c2dd09efff547679250cd495b63

build-binary: fix finding multiple orig tarballs
Turns out, I forgot that the format for additional orig tarball is
".orig-<part name>.tar.*", not ".orig.<part name>.tar.*". Thus, the
find rule doesn't work.

This commit fixes the find rule so that it correctly includes the part
tarball as well.

While we're at it, fix the comment too.

build-binary.sh
@@ -67,9 +67,10 @@ if [ -f multidist.buildinfo ]; then
 	rootwp=$(pwd)
 	export rootwp
 
-	# Move orig to mbuild folder
+	# Links orig to mbuild folder
 	find "$rootwp" \
-        -maxdepth 1 -type f -name '*.orig.*' \
+        -maxdepth 1 -type f \
+		'(' -name '*.orig.*' -o -name '*.orig-*.*' ')' \
         -exec mv '{}' "$rootwp/mbuild" ';'
 
 	for d in $MULTI_DIST ; do

commit 138d0200e09fc882f0af473f178bcceeaea74c79

build-binary: fix finding multiple orig tarballs, part 2
Well, silly me. The part I touched "correctly" moves, not link. The
linking is the second find, and I didn't fix it. So, fix it in this
commit, and also revert the comment change.

🤦

Fixes: efe9052f ("build-binary: fix finding multiple orig tarballs")

build-binary.sh
@@ -67,7 +67,7 @@ if [ -f multidist.buildinfo ]; then
 	rootwp=$(pwd)
 	export rootwp
 
-	# Links orig to mbuild folder
+	# Moves orig to mbuild folder
 	find "$rootwp" \
         -maxdepth 1 -type f \
 		'(' -name '*.orig.*' -o -name '*.orig-*.*' ')' \
@@ -92,7 +92,8 @@ if [ -f multidist.buildinfo ]; then
 
 		# Ensure orig tarball exists
 		find ../ \
-			-maxdepth 1 -type f -name '*.orig.*' \
+			-maxdepth 1 -type f \
+			'(' -name '*.orig.*' -o -name '*.orig-*.*' ')' \
 			-exec ln -s '{}' ./ ';'
 
 		/usr/bin/build-and-provide-package